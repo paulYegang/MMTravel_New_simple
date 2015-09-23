2060bf72acc66da0579c31b35db9a4ef222d0daa
super <super@360-hello.local>
2015-09-23 15:02:28 +0800

    Initial Commit

---
+++ MMTravel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMTravel
+//
+//  Created by super on 15/9/23.
+//  Copyright (c) 2015年 super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MMTravel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MMTravel
+//
+//  Created by super on 15/9/23.
+//  Copyright (c) 2015年 super. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
